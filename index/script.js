31b171b900301d761ada4398ca3d6a39761d57f2
adem sliti <houtaads@gmail.com>
2025-03-16 14:47:22 +0100

    hello

---
+++ index/script.js
@@ -0,0 +1,56 @@
+//alert("hello my friend")
+ //confirm(" what's your name?")
+// console.log("hello my friend")
+//prompt("hi")
+//console.log(Boolean(undefined))
+//console.log(Boolean(null))
+//console.log(Boolean(NaN))
+//console.log(Boolean(""))
+//console.log(Boolean(0))
+//console.log(Boolean(false))
+//var a="my name is"
+//b=24
+//console.log(`${a} hama and my age is${b}`)
+//function add(a,b) {
+    //return a-b
+    
+    
+    //}
+    //console.log(add(5,10))
+    //var storage=[1,"monday",null]
+    //console.log(storage[0])
+    //console.log(storage[6])
+    //console.log(storage)
+    //storage.shift()
+    //storage.push(10)
+   // storage.unshift("hello")
+    //storage.pop()
+    //console.log(storage)
+    var bmw={
+        color:"red",
+        year:1975,
+        owner:"ahmed"
+    }
+    var mercedas={
+        color:"red",
+        year: 1976,
+        owner:"adem"
+    }
+    var audi={
+        color:"red",
+        year:1978,
+        owner:"rayen"
+    }
+    var gimli={
+        name:"gimli",
+        race:"dwarf",
+        weapon:"axe",
+        greet:function(){
+            return `hi,my name is ${this.name}!`
+        }
+
+
+    }
+    gimli.age=100
+    console.log(gimli)
+    
